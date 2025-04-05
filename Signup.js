a7e378dedcbe340abb42bd7d70f2b450ad7b1fcb
Aswathi04 <150355256+Aswathi04@users.noreply.github.com>
2025-04-05 13:30:33 +0530

    src

---
+++ Signup.js
@@ -0,0 +1,35 @@
+// Signup.js
+import { useState } from "react";
+import { useAuth } from "./AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { SignUp } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await SignUp(email, password);
+      alert("Signup Successful!");
+      navigate("/"); // Redirect to home page
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSignup}>
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
